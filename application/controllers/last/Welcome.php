55fc938b637dbd411369991023d9b60115989cde
Aaron Freytag <aaroncfreytag@gmail.com>
2017-02-02 17:35:55 -0800

    Added last/Welcome.php controller for Isaac Asimov's quote

---
+++ application/controllers/last/Welcome.php
@@ -0,0 +1,21 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Welcome extends Application
+{
+    function __construct()
+    {
+        parent::__construct();
+    }
+    
+    public function index()
+    {
+        $this->data['pagebody'] = 'justone';
+        $source = $this->quotes->get('6');
+        $this->data['who'] = $source['who'];
+        $this->data['what'] = $source['what'];
+        $this->data['mug'] = $source['mug'];
+        $this->render();
+    }
+}
